df89ca0dc46439e87dcb7769f9838504fa6f00c3
Eric Lindow <elindow@verizon.net>
2016-11-18 20:17:25 -0500

    seed questions

--- Gemfile
+++ Gemfile
@@ -71,6 +71,8 @@ gem "fabrication"
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
 end
 
+gem 'rails_12factor', group: :production
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+questions = Question.create([{num: 500, page: 500, qtext: "test question"}])
